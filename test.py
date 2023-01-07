ce543103879ac642bc4271806be44f7539d32a97
Endiasz <endiaszpl@gmail.com>
2023-01-07 17:06:48 +0100

    a

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import hashlib
 
 # Define the string and salt
 string = "nuBz6Bdhn"
-salt = "j3bAdelk0"
+salt = "0337_t"
 
 # Convert the string and salt to bytes
 string_bytes = string.encode()
@@ -11,3 +11,6 @@ salt_bytes = salt.encode()
 hashed_string = hashlib.pbkdf2_hmac("sha1", string_bytes, salt_bytes, 4096)
 hashed_string_hex = hashed_string.hex()
 print(hashed_string_hex)
+
+
+
