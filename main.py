97b2fde34cf4e9a26b69775bda66f2c4a209a514
Endiasz <endiaszpl@gmail.com>
2023-01-22 13:51:11 +0100

    ugh

--- main.py
+++ main.py
@@ -1,104 +1,119 @@
+import tkinter as tk
 import binascii
 import hashlib
-import pymysql
+import csv
 
 
-print("=================================================")
-print("=================================================")
-print("----------------- Nice program ------------------")
-print("=================================================")
-print("=================================================")
-
-#  user input
-sklep_id = int(input('Pass shop id '))
-while sklep_id < 1:
-    input('Pass corect value, only numbers')
-
-user_id = int(input('Pass computer id '))
-while (user_id < 0 and user_id > 7):
-    user_id = int(input('Pass computer id (betwen 1 and 6)'))
-
-print('Using database to search for info')
-
-# db side
-try:
-    conn = pymysql.connect(user="root", password="",
-                           host="localhost", database="sklep")
-
-except:
-    print("Failed to get to database")
-    input("Pres enter to try again")
-    exit()
-
-cursor = conn.cursor()
-
-cursor.execute("SELECT * FROM sklep WHERE id=%s", (sklep_id,))
-result = cursor.fetchone()
-conn.close()
-
-print(result)
-
-# check if data is avaible 
-if result is None:
-    print("ID not found!")
-    input("Press enter to exit and try again")
-    exit()
-else:
+# Create the main window
+window = tk.Tk()
+window.title("Configurator")
+window.geometry("300x200")
 
 
-    router_ip = result[1]
-    octets = router_ip.split(".")
-    octets[3] = str(user_id+ 239)
-    device_ip = ".".join(octets)
+# Create the input fields
+id_sklep_label = tk.Label(window, text="ID Sklepu:")
+id_sklep = tk.Spinbox(window)
+id_dev_label = tk.Label(window, text="ID drukarki:")
+id_dev = tk.Spinbox(window, from_=1, to=6)
 
-    formatted_shop_id = "{:04d}".format(sklep_id)
-    # print(formatted_shop_id)
+# Create the output label
+output_label = tk.Label(window, text="")
+
+# Create a function to compute the output
 
 
-    # wps psk
-    def wpa_psk(ssid, password):
-        dk = hashlib.pbkdf2_hmac(
-            'sha1',
-            str.encode(password),
-            str.encode(ssid),
-            4096,
-            256
-        )
-        return (binascii.hexlify(dk))
+def get_values():
+    # Get the values from the input fields
+    sklep_id = str(id_sklep.get())
+    user_id = str(id_dev.get())
+    formatted_shop_id = "{:04d}".format(int(sklep_id))
+    # formatted_shop_id+='_t'
 
-    wpa_psk_coded = (wpa_psk(formatted_shop_id, result[3])[0:64].decode('utf8'))
-    # wpa_psk_coded = (wpa_psk(formatted_shop_id, "nuBz")[0:64].decode('utf8'))
-
-    print('\n Text to copy\n')
-
+    # Convert the input to numbers (if possible)
+    result = None
     try:
-
-        complete_data = f'''! U1 setvar "ip.dhcp.enable" "off"
-        ! U1 setvar "wlan.ip.protocol" "permanent"
-        ! U1 setvar "device.friendly_name" "ZEBRA_{formatted_shop_id}_{user_id}"
-        ! U1 setvar "ip.addr" "{device_ip}"   
-        ! U1 setvar "ip.netmask" "255.255.255.0"
-        ! U1 setvar "ip.gateway" "{result[1]}"  
-        ! U1 setvar "wlan.essid" "{formatted_shop_id}_t"   
-        ! U1 setvar "wlan.leap_mode" "off"
-        ! U1 setvar "wlan.kerberos.mode" "off"
-        ! U1 setvar "wlan.encryption_mode" "off"
-        ! U1 setvar "wlan.8021x.enable" "off"
-        ! U1 setvar "wlan.operating_mode" "infrastructure"
-        ! U1 setvar "wlan.user_channel_list" "36,38,40,42,44,46,48,52,56,60,64,100,104,108,112,116,120,124,128,132,136,140"
-        ! U1 setvar "wlan.country_code" "europe"
-        ! U1 setvar "wlan.allowed_band" "5"
-        ! U1 setvar "wlan.international_mode" "off"
-        ! U1 setvar "wlan.wpa.authentication" "psk"
-        ! U1 setvar "wlan.wpa.enable" "on"
-        ! U1 setvar "wlan.wpa.psk" "{wpa_psk_coded}"   
-        ! U1 do "device.reset" ""\n'''
-
-        print(complete_data)
-
+        # Open the CSV file
+        with open('data.csv', 'r') as csv_file:
+          # Create a CSV reader object
+          reader = csv.reader(csv_file)
+        #   row_number = sklep_id
+          # Loop through the rows
+          for row in reader:
+            if(row[0]==formatted_shop_id):
+                result = row
+                print(result)
+                break
     except:
-        print("Error while asembling output string")
-    input("Press enter to exit")
+        output_label.config(text="Nie otwieranie pliku, sprawdź nazwę pliku, plik powinien nazywać się data.csv")
+        exit()
+
+    if result == None:
+        output_label.config(text="Nie ma takiego id")
+    else:
+
+        router_ip = result[1]
+        octets = router_ip.split(".")
+        octets[3] = str(int(user_id) + 239)
+        device_ip = ".".join(octets)
 
 
-    #  Dorobić żeby wiedziało jak user udczyta nieistniejący rekord
+        # wps psk
+        string_bytes = result[3].encode()
+        salt_bytes = (formatted_shop_id+'_t').encode()
+
+        hashed_string = hashlib.pbkdf2_hmac("sha1", string_bytes, salt_bytes, 4096,64)
+
+        wpa_psk_coded_128 = hashed_string.hex()
+        # length = len(string)
+        wpa_psk_coded = wpa_psk_coded_128[:len(wpa_psk_coded_128) // 2]
+        try:
+
+            complete_data = f'''
+! U1 setvar "ip.dhcp.enable" "off"
+! U1 setvar "wlan.ip.protocol" "permanent"
+! U1 setvar "device.friendly_name" "ZEBRA_{formatted_shop_id}_0{user_id}"
+! U1 setvar "ip.addr" "{device_ip}"   
+! U1 setvar "ip.netmask" "255.255.255.0"
+! U1 setvar "ip.gateway" "{result[1]}"  
+! U1 setvar "wlan.essid" "{formatted_shop_id}_t"   
+! U1 setvar "wlan.leap_mode" "off"
+! U1 setvar "wlan.kerberos.mode" "off"
+! U1 setvar "wlan.encryption_mode" "off"
+! U1 setvar "wlan.8021x.enable" "off"
+! U1 setvar "wlan.operating_mode" "infrastructure"
+! U1 setvar "wlan.user_channel_list" "36,38,40,42,44,46,48,52,56,60,64,100,104,108,112,116,120,124,128,132,136,140"
+! U1 setvar "wlan.country_code" "europe"
+! U1 setvar "wlan.allowed_band" "5"
+! U1 setvar "wlan.international_mode" "off"
+! U1 setvar "wlan.wpa.authentication" "psk"
+! U1 setvar "wlan.wpa.enable" "on"
+! U1 setvar "wlan.wpa.psk" "{wpa_psk_coded.upper()}"   
+! U1 do "device.reset" ""\n'''
+
+            output_window = tk.Toplevel(window)
+            output_window.title("Do skopiowania")
+            output_window.geometry("1000x500")
+
+    # Create a label to display the output in the new window
+            output_text = tk.Text(output_window,  height=1000, width=500)
+            output_text.pack()
+            output_text.insert(tk.END, complete_data)
+
+        except:
+            output_label.config(text="Error przy tworzeniu tekstu")
+            input("Kliknij enter aby kontynuowac")
+
+# Create a button to trigger the computation
+get_output = tk.Button(window, text="Compute", command=get_values)
+
+# Place the input fields and button in the window
+id_sklep_label.pack()
+id_sklep.pack()
+id_dev_label.pack()
+id_dev.pack()
+
+get_output.pack()
+output_label.pack()
+
+# Run the main loop
+window.mainloop()
---
+++ tesssssssssssssssssssssssssst.py
@@ -0,0 +1,21 @@
+import csv
+
+
+# Open the CSV file
+with open('sklep.csv', 'r') as csv_file:
+  # Create a CSV reader object
+  reader = csv.reader(csv_file)
+  number = 10
+  row_number = 10
+  # Loop through the rows
+  for row in reader:
+    # If the row number is the one you want
+    if reader.line_num == row_number:
+      # Loop through the columns in the row
+      for column in row:
+        # If the column value is the number you are looking for
+        if column == number:
+            result = row.split(",")
+            # dodać walidacje id 
+            break
+      break
